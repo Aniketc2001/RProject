901404b9b9a4db9954387060912ae7592efc9cde
Aniketc2001 <108540622+Aniketc2001@users.noreply.github.com>
2022-10-05 20:36:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import MainP from './Components/MainP';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <MainP />
+    </div>
+  );
+}
+
+export default App;
